af2d8c63368ba38bc28669dfbed5d88367d5d809
guiaugh <g.augustooliv200@gmail.com>
2025-03-20 20:39:20 -0300

    aula 05 fundamentos

---
+++ 05-fundamentos-react-native/App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View } from 'react-native';
+import PrimeiroComponente from './componentes/PrimeiroComponente';
+import SegundoComponente from './componentes/SegundoComponente';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <PrimeiroComponente/>
+      <SegundoComponente/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
