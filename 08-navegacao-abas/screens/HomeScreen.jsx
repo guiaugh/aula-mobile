43c3c3af1325b126db50b8185bdceaeda181bebf
guiaugh <g.augustooliv200@gmail.com>
2025-04-10 19:37:14 -0300

    Merge com o do professor

--- 08-navegacao-abas/screens/HomeScreen.jsx
+++ 08-navegacao-abas/screens/HomeScreen.jsx
@@ -1,12 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { Card, Paragraph, Text, Title } from 'react-native-paper'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+
+    <ScrollView>
+      <View style={styles.container}>
+
+        <Text variant='headlineSmall' style={{ fontWeight: 'bold' }}>Início</Text>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
+
+
+      </View>
+
+    </ScrollView>
+
+
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'fff',
+    alignItems: 'center',
+    paddingTop: 10
+  }
+})
