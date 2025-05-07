14b9a3d5c5fa590028798127a5b0926820c1a2c4
guiaugh <g.augustooliv200@gmail.com>
2025-05-07 20:54:13 -0300

    Navegação Stack e Drawer

---
+++ 10-navegacao-drawer/src/screens/HomeScreen.jsx
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Button } from 'react-native-paper'
+
+export default function HomeScreen(navigation, route) {
+  return (
+    <View>
+      <Text>HomeScreen</Text>
+      <Button
+        mode='contained'
+        onPress={() => navigation.openDrawer()}
+      >
+        Abrir Drawer
+      </Button>
+
+      <Button
+        mode='contained'
+        onPress={() => {
+          navigation.openDrawer()
+          setTimeout(() => {
+            navigation.closeDrawer
+          }, 3000)
+        }} >
+        Abrir Drawer e fechar
+      </Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
